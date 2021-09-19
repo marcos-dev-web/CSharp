4a1e9a6e1276c64f617e92c91fc6849fc5f7f09d
Marcos Venicius <76922074+marcos-dev-web@users.noreply.github.com>
2021-09-19 14:56:29 -0300

    test with dynamic type

---
+++ Tests1/Tests1/List.cs
@@ -0,0 +1,63 @@
+﻿
+namespace Tests1 {
+    class List<T> {
+
+        private T[] _array;
+        public List() {}
+
+        public List(T[] array) {
+            _array = array;
+        }
+
+        private bool AreEqual( int currentIndex ) {
+            if ( _array.Length - 1 < currentIndex ) {
+                T e1 = _array[currentIndex];
+                T e2 = _array[currentIndex + 1];
+
+                bool r = e1.Equals(e2);
+
+                return r;
+            }
+            else {
+                T e1 = _array[currentIndex];
+                T e2 = _array[^2];
+
+                bool r = e1.Equals(e2);
+
+                return r;
+            }
+        }
+
+        public bool ArrayIsEqual() {
+            if (_array == null) {
+                return false;
+            }
+
+            if ( _array.Length == 1 ) {
+                return true;
+            }
+
+            for ( int i = 0; i < _array.Length; i++ ) {
+                if ( !AreEqual(i) ) {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        public bool ArrayIsEqual( T[] array ) {
+            _array = array;
+
+            if ( array.Length == 1 ) {
+                return true;
+            }
+
+            for ( int i = 0; i < array.Length; i++ ) {
+                if ( !AreEqual(i) ) {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}
---
+++ Tests1/Tests1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+// this program is to can verify if an array has
+// all of your items equals
+
+// i know that is possible to do that with less code, but it's only to study
+namespace Tests1 {
+    class Program {
+        static void Main() {
+
+            List<int> List1 = new List<int>(new int[] { 10, 10, 10 });
+
+            Console.WriteLine($"List1 is all equal: {List1.ArrayIsEqual()}");
+
+            List<string> List2 = new List<string>();
+
+            Console.WriteLine($"List2 is all equal: {List2.ArrayIsEqual(new string[] { "marcos", "Marcos" })}");
+
+        }
+    }
+}
