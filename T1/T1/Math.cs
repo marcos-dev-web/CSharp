aea394e4c01efed42d514bc70f5be2a4b2ae58ff
Marcos Venicius <76922074+marcos-dev-web@users.noreply.github.com>
2021-10-12 17:27:44 -0300

    T1

---
+++ T1/T1/Math.cs
@@ -0,0 +1,33 @@
+﻿using System.Globalization;
+
+namespace T1 {
+    internal class Math {
+        private double _number;
+
+        internal Math( double number ) {
+            _number = number;
+        }
+        internal double Number {
+            get => _number;
+            set {
+                if ( value > 0 && value < 10 ) {
+                    _number = value;
+                }
+            }
+        }
+
+        internal Math Increment( double value ) {
+            _number += value;
+            return this;
+        }
+
+        internal Math Decrement( double value ) {
+            _number -= value;
+            return this;
+        }
+
+        public override string ToString() {
+            return _number.ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+}
---
+++ T1/T1/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Threading;
+
+namespace T1 {
+    public class Program {
+        static void Main() {
+            Console.Clear();
+            Console.BackgroundColor = ConsoleColor.Black;
+            Console.ForegroundColor = ConsoleColor.DarkGreen;
+            double start = 10;
+
+            var math = new Math(start)
+            {
+                Number = 20
+            };
+
+            Console.WriteLine(math); // 10
+
+            math.Number = 5;
+
+            Console.WriteLine(math); // 5
+
+            math.Increment(10).Increment(10).Increment(10).Decrement(5);
+
+            Console.WriteLine(math); // 30
+
+            Beep(1, 20);
+        }
+
+        private static void Beep( int time, int max ) {
+            int timer = max * 50;
+
+            if ( (timer - time * 100) > 0 ) {
+                timer = max * 50 - (time * 100);
+            }
+            else {
+                timer = time;
+            }
+
+            Thread.Sleep(timer);
+            Console.Beep();
+
+            if ( time < max ) {
+                Beep(time + 1, max);
+            } else {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.BackgroundColor = ConsoleColor.White;
+                Console.WriteLine("\n\nBooooooooooommmm!!!!!\n\n");
+                Console.ResetColor();
+            }
+        }
+    }
+}
